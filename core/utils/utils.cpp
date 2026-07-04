9be0d2269d82430038c40b7ab210761e10ac61e9
khainguyen <khainguyendiep1107@gmail.com>
2026-07-04 11:16:34 +0700

    feat: add timezone at the end of field event_time

--- core/utils/utils.cpp
+++ core/utils/utils.cpp
@@ -1,8 +1,4 @@
 #include "utils.h"
-#include <pcap.h>
-#include <chrono>
-#include <sstream>
-#include <iomanip>
 
 std::string get_timestamp() {
     auto now = std::chrono::system_clock::now();
@@ -17,11 +13,21 @@ std::string format_timestamp_from_header(const struct pcap_pkthdr *header) {
 	struct tm tm_info;
     // tv_sec is time_t — transfer to tm struct to format
 	localtime_r(&header->ts.tv_sec, &tm_info);
-    char buf[32];
-    strftime(buf, sizeof(buf), "%Y-%m-%dT%H:%M:%S", &tm_info);
-    // Add microseconds to the end of string
-    char full[40];
-    snprintf(full, sizeof(full), "%s.%06ld", buf, (long)header->ts.tv_usec);
-    return std::string(full);
-    // Answer: "2025-01-01T13:25:30.123456Z"
+    char date_time_buf[32];
+    strftime(date_time_buf, sizeof(date_time_buf), "%Y-%m-%dT%H:%M:%S", &tm_info);
+
+	//get timezone in format "+0700"
+	char tz_buf[8];
+    std::strftime(tz_buf, sizeof(tz_buf), "%z", &tm_info); // tz_buf has format "+0700"
+
+    // Format tz__buf to "+07:00" (add colon)
+    char formatted_tz[10] = "Z"; // default timezone is "Z" (UTC 0), if cannot get the timezone
+    if (tz_buf[0] != '\0' && (tz_buf[0] == '+' || tz_buf[0] == '-')) {
+        std::snprintf(formatted_tz, sizeof(formatted_tz), "%c%.2s:%.2s", tz_buf[0], &tz_buf[1], &tz_buf[3]);
+    }
+
+    // reassemble all
+    char full_timestamp[64];
+    std::snprintf(full_timestamp, sizeof(full_timestamp), "%s.%06ld%s", date_time_buf, (long)header->ts.tv_usec, formatted_tz);
+    return std::string(full_timestamp);
 }
